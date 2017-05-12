75d7b3928b3aaec1a3572e9bc4889fbaaa33086c
KrystianZakrys <krystian.zakrys@gmail.com>
2017-05-13 01:00:39 +0200

    Basic functionality without file I/O. Just simple editor with basic text formating

---
+++ BetterNotepad/BetterNotepad/Format.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BetterNotepad
+{
+    /// <summary>
+    /// Interaction logic for Format.xaml
+    /// </summary>
+    public partial class Format : Window
+    {
+        public Format()
+        {
+            InitializeComponent();
+            Loaded += MyWindow_Loaded;
+            this.SizeToContent = SizeToContent.WidthAndHeight;
+            slider_size.Value = 9;
+        }
+
+        private void MyWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            size_label.Content = "Size: " + (int)(double)Properties.Settings.Default["Size"];
+            slider_size.Value = (double)Properties.Settings.Default["Size"];
+
+            cb_fontFamily.SelectedValue = Properties.Settings.Default["FontFamily"];
+
+            chckBox_bold.IsChecked = (bool)Properties.Settings.Default["Bold"];
+            chckBox_italic.IsChecked = (bool)Properties.Settings.Default["Italic"];
+        }
+
+        private void btn_save_format_Click(object sender, RoutedEventArgs e)
+        {
+            Properties.Settings.Default["Size"] = example_label.FontSize;
+            Properties.Settings.Default["FontFamily"] = example_label.FontFamily;
+            Properties.Settings.Default["Bold"] = (example_label.FontWeight == FontWeights.Bold) ? true : false;
+            Properties.Settings.Default["Italic"] = (example_label.FontStyle == FontStyles.Italic) ? true : false;
+            Properties.Settings.Default.Save();
+            ((MainWindow)Application.Current.MainWindow).reloadSettings();
+        }
+
+        private void slider_size_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            size_label.Content = "Size: " + (int)slider_size.Value;
+            example_label.FontSize = slider_size.Value;
+        }
+
+        private void cb_fontFamily_Selected(object sender, RoutedEventArgs e)
+        {
+            example_label.FontFamily = (FontFamily)((ComboBox)sender).SelectedValue;
+        }
+
+        private void chckBox_bold_Checked(object sender, RoutedEventArgs e)
+        {
+            example_label.FontWeight = FontWeights.Bold;
+        }
+
+        private void chckBox_italic_Checked(object sender, RoutedEventArgs e)
+        {
+            example_label.FontStyle = FontStyles.Italic;
+        }
+
+        private void chckBox_bold_Unchecked(object sender, RoutedEventArgs e)
+        {
+            example_label.FontWeight = FontWeights.Normal;
+        }
+
+        private void chckBox_italic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            example_label.FontStyle = FontStyles.Normal;
+        }
+
+        private void btn_cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ BetterNotepad/BetterNotepad/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BetterNotepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommandBindings.Add(new CommandBinding(ApplicationCommands.Close,
+                new ExecutedRoutedEventHandler(delegate (object sender, ExecutedRoutedEventArgs args) { this.Close(); })));
+        }
+        public void DragWindow(object sender, MouseButtonEventArgs args)
+        {
+            DragMove();
+        }
+
+
+        private void Opacity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            rtb_note.Opacity = opacity_slider.Value;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.WindowState == WindowState.Maximized)
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+
+        private void btn_options_Click(object sender, RoutedEventArgs e)
+        {
+            Options opt_window = new Options();
+            opt_window.ShowDialog();
+        }
+
+        private void btn_format_Click(object sender, RoutedEventArgs e)
+        {
+            Format format_window = new Format();
+            format_window.ShowDialog();
+        }
+
+        public void reloadSettings()
+        {
+            rtb_note.FontFamily = (FontFamily)Properties.Settings.Default["FontFamily"];
+            rtb_note.FontSize = (double)Properties.Settings.Default["Size"];
+            rtb_note.FontWeight = ((bool)Properties.Settings.Default["Bold"]) ? FontWeights.Bold : FontWeights.Normal;
+            rtb_note.FontStyle = ((bool)Properties.Settings.Default["Italic"]) ? FontStyles.Italic : FontStyles.Normal;
+        }
+    }
+}
